ec129848413c4a725c0c0bf513834fd2fef90c31
Rehan Sheikh <rs2126@cam.ac.uk>
2021-07-28 13:37:19 +0100

    Added deployment of Timer contract

--- tests/test_lottery.py
+++ tests/test_lottery.py
@@ -1,18 +1,41 @@
 import time
 import pytest
-from brownie import Lottery, VRFConsumer, convert, network
+from brownie import Lottery, VRFConsumer, Timer, convert, network
 from scripts.helpful_scripts import (
     get_account,
     get_contract,
     LOCAL_BLOCKCHAIN_ENVIRONMENTS,
 )
 
+SIZE_OF_LOTTERY = 6
+MAX_VALID_NUMBER = 30
+FEE = 100_000_000_000_000_000
+
 @pytest.fixture
-def deploy_lottery_contract():
+def deploy_timer_contract():
 
-    # Arrange
-    lottery = Lottery.deploy()
+    if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        timer = Timer.deploy()
+        assert timer is not None
+        return timer
+    else:
+        return
 
+
+@pytest.fixture
+def deploy_lottery_contract(deploy_timer_contract):
+
+    # Arrange / Act
+    timer = deploy_timer_contract
+
+    lottery = Lottery.deploy(
+        SIZE_OF_LOTTERY,
+        MAX_VALID_NUMBER,
+        FEE,
+        timer.address,
+        {"from": get_account()})
+
+    # Assert
     assert lottery is not None
     return lottery
 
