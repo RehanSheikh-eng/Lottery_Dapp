8589b695712cae57d92022959f1508891c9842fd
Rehan Sheikh <rs2126@cam.ac.uk>
2021-07-28 14:01:56 +0100

    Added fixture to deploy randomness provider

--- tests/test_lottery.py
+++ tests/test_lottery.py
@@ -12,31 +12,59 @@ MAX_VALID_NUMBER = 30
 FEE = 100_000_000_000_000_000
 
 @pytest.fixture
+def deploy_vrfconsumer_contract(get_keyhash, chainlink_fee):
+
+    # Arrange / Act
+    vrf_consumer = VRFConsumer.deploy(
+        get_keyhash,
+        get_contract("vrf_coordinator").address,
+        get_contract("link_token").address,
+        chainlink_fee,
+        {"from": get_account()})
+    
+    # Provide VRFConsumer with funds
+    tx = get_contract("link_token").transfer(
+        vrf_consumer.address, chainlink_fee * 3, {"from": get_account()}
+    )
+
+    # Assert
+    assert vrf_consumer is not None
+    assert tx is not None
+    return vrf_consumer
+
+
+@pytest.fixture
 def deploy_timer_contract():
 
     if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
         timer = Timer.deploy()
         assert timer is not None
-        return timer
+        return timer.address
     else:
-        return
+        return 0
 
 
 @pytest.fixture
-def deploy_lottery_contract(deploy_timer_contract):
+def deploy_lottery_contract(deploy_timer_contract, deploy_vrfconsumer_contract):
 
     # Arrange / Act
-    timer = deploy_timer_contract
+    timer_address = deploy_timer_contract
+    vrfconsumer = deploy_vrfconsumer_contract
 
     lottery = Lottery.deploy(
         SIZE_OF_LOTTERY,
         MAX_VALID_NUMBER,
         FEE,
-        timer.address,
+        timer_address,
         {"from": get_account()})
+
+    tx = lottery.initialize(vrfconsumer.address)
 
     # Assert
     assert lottery is not None
+    assert tx is not None
+
+    # Return
     return lottery
 
 def  test_can_start_lottery(deploy_lottery_contract):
