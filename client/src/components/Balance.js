f53788d55488754025cb43c10f20460eee29b05e
Rehan Sheikh <rs2126@cam.ac.uk>
2021-08-13 15:11:42 +0100

    Added basic styling to balance component

--- client/src/components/Balance.js
+++ client/src/components/Balance.js
@@ -2,7 +2,27 @@ import React from "react";
 import { useEffect, useState } from "react";
 import loadContract from "../utils/loadContract";
 import { ethers } from 'ethers'
+import { Container } from "@material-ui/core";
+import { Typography } from "@material-ui/core";
+import { makeStyles } from '@material-ui/core/styles';
+import { lineHeight } from "@material-ui/system";
 
+
+const useStyles = makeStyles({
+    root: {
+      flexGrow: 1,
+    },
+
+    balanceContainer: {
+        background: "linear-gradient(rgb(118, 69, 217) 0%, rgb(69, 42, 122) 100%)",
+    },
+
+    balanceText:{
+        color: "rgb(253, 171, 50)",
+        lineHeight: 1.5,
+        fontWeight: 600,
+    }  
+  });
 
 export default function Balance(){
 
@@ -11,9 +31,10 @@ export default function Balance(){
     const [usdbalance, setUsdBalance] = useState(0);
     const [lottery, setLottery] = useState();
     const [price, setPrice] = useState("0.00");
-    const [lastfetch, setLastFetch] = useState("")
+    const [lastfetch, setLastFetch] = useState("");
 
-    const MINUTE_MS = 10000;
+    const classes = useStyles();
+    const MINUTE_MS = 30000;
     const URL = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=ethereum"
 
     useEffect(async () => {
@@ -26,19 +47,20 @@ export default function Balance(){
 
         addLotteryContractListner();
 
-        const interval = setInterval(function fetchPrice(){
+        function fetchPrice(){
             fetch(URL)
             .then(response => {
                 return response.json();
             })
             .then(data => {
                 console.log(typeof ethbalance);
+                console.log(data[0].current_price);
                 const usdbalance = ethers.utils.formatEther(ethbalance) * (data[0].current_price);
                 setUsdBalance(usdbalance);
             })
-          }(), MINUTE_MS);
-
-
+        }
+        fetchPrice();
+        const interval = setInterval(fetchPrice, MINUTE_MS);
 
         console.log(ethbalance);
         console.log(typeof ethbalance);
@@ -69,11 +91,25 @@ export default function Balance(){
     }    
 
     return(
-        <div>
-            <h1>{ethers.utils.formatEther(ethbalance)}</h1>
-            <h1>{usdbalance.toString()}</h1>
-        </div>
+        <Container maxWidth="lg" className={classes.balanceContainer}>
+            <Typography align="center" variant="h2"> The Speed Lottery </Typography>
+            <div>
+                <Typography 
+                    align="center"
+                    variant="h6"
+                    className={classes.balanceText}
+                > 
+                    {ethers.utils.formatEther(ethbalance)}
+                </Typography>
 
-
+                <Typography
+                    align="center"
+                    variant="h6"
+                    className={classes.balanceText}
+                > 
+                    ${usdbalance.toString()}
+                </Typography>
+            </div>
+        </Container>
     );
 }
