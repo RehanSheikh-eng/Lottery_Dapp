3e6d529ddf56d617e4e705fd945ce72f2975f98c
Rehan Sheikh <rs2126@cam.ac.uk>
2021-08-29 02:05:17 +0100

    Updates

---
+++ precache-manifest.f91984966cfdd56725756f5b91ba1170.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b96cf3a6f7b4afcab4131e44fe461d4",
+    "url": "/Lottery_Dapp/index.html"
+  },
+  {
+    "revision": "2e29b67cce487c0795e4",
+    "url": "/Lottery_Dapp/static/css/main.03ce8a2b.chunk.css"
+  },
+  {
+    "revision": "475d6067e064d9629ede",
+    "url": "/Lottery_Dapp/static/js/2.ba721803.chunk.js"
+  },
+  {
+    "revision": "9a194aad58abb004d4ca41cc9cb93c37",
+    "url": "/Lottery_Dapp/static/js/2.ba721803.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9887832f47feabb8f5c4",
+    "url": "/Lottery_Dapp/static/js/3.79141eeb.chunk.js"
+  },
+  {
+    "revision": "c5e202dcb35d716a43a3",
+    "url": "/Lottery_Dapp/static/js/4.e17b7d95.chunk.js"
+  },
+  {
+    "revision": "ee86bb9dc76af77405bd",
+    "url": "/Lottery_Dapp/static/js/5.c2647709.chunk.js"
+  },
+  {
+    "revision": "8119f977843375a0138d",
+    "url": "/Lottery_Dapp/static/js/6.a2235ca8.chunk.js"
+  },
+  {
+    "revision": "2e29b67cce487c0795e4",
+    "url": "/Lottery_Dapp/static/js/main.b59b893a.chunk.js"
+  },
+  {
+    "revision": "edf1f1ef79d6ef1131b7",
+    "url": "/Lottery_Dapp/static/js/runtime-main.6f6baf9e.js"
+  },
+  {
+    "revision": "32555e89db2fb65ee5bf5973943ca43f",
+    "url": "/Lottery_Dapp/static/media/tennis-ball.32555e89.svg"
+  }
+]);
